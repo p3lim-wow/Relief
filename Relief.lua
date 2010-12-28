e8401bc8521482072d801ad7d92b58de5edfd719
p3lim <adrianlund@gmail.com>
2010-12-28 07:46:09 +0100

    Do some housecleaning

--- Relief.lua
+++ Relief.lua
@@ -12,41 +12,18 @@
 local TEXTURE = [=[Interface\ChatFrame\ChatFrameBackground]=]
 
 local parent = CreateFrame('Frame')
-parent:SetScript('OnEvent', function(self, event) self[event](Minimap) end)
 parent:RegisterEvent('PLAYER_LOGIN')
+parent:SetScript('OnEvent', function()
+	Minimap:ClearAllPoints()
+	Minimap:SetParent(UIParent)
+	Minimap:SetPoint('TOPRIGHT', -20, -20)
+	Minimap:SetBackdrop({bgFile = TEXTURE, insets = {top = -1, bottom = -1, left = -1, right = -1}})
+	Minimap:SetBackdropColor(0, 0, 0)
+	Minimap:SetMaskTexture(TEXTURE)
+	Minimap:SetScale(0.9)
 
-for _, object in pairs({
-	BattlegroundShine,
-	GameTimeFrame,
-	MinimapBorder,
-	MinimapBorderTop,
-	MinimapNorthTag,
-	MinimapZoomIn,
-	MinimapZoomOut,
-	MinimapZoneTextButton,
-	MiniMapBattlefieldBorder,
-	MiniMapLFGFrameBorder,
-	MiniMapTracking,
-	MiniMapWorldMapButton,
-}) do
-	if(object:GetObjectType() == 'Texture') then
-		object:SetTexture(nil)
-	else
-		object:Hide()
-	end
-end
-
-function parent:PLAYER_LOGIN()
-	self:ClearAllPoints()
-	self:SetParent(UIParent)
-	self:SetPoint('TOPRIGHT', -20, -20)
-	self:SetBackdrop({bgFile = TEXTURE, insets = {top = -1, bottom = -1, left = -1, right = -1}})
-	self:SetBackdropColor(0, 0, 0)
-	self:SetMaskTexture(TEXTURE)
-	self:SetScale(0.9)
-
-	self:EnableMouseWheel()
-	self:SetScript('OnMouseWheel', function(self, direction)
+	Minimap:EnableMouseWheel()
+	Minimap:SetScript('OnMouseWheel', function(self, direction)
 		self:SetZoom(self:GetZoom() + (self:GetZoom() == 0 and direction < 0 and 0 or direction))
 	end)
 
@@ -59,15 +36,36 @@ function parent:PLAYER_LOGIN()
 	end)
 
 	MiniMapLFGFrame:ClearAllPoints()
-	MiniMapLFGFrame:SetParent(self)
+	MiniMapLFGFrame:SetParent(Minimap)
 	MiniMapLFGFrame:SetPoint('TOPRIGHT')
 	MiniMapLFGFrame:SetHighlightTexture(nil)
 
 	MiniMapBattlefieldFrame:ClearAllPoints()
-	MiniMapBattlefieldFrame:SetParent(self)
+	MiniMapBattlefieldFrame:SetParent(Minimap)
 	MiniMapBattlefieldFrame:SetPoint('TOPRIGHT')
 
 	MiniMapInstanceDifficulty:UnregisterAllEvents()
 	MiniMapMailFrame:UnregisterAllEvents()
 	MinimapCluster:EnableMouse(false)
+
+	for _, object in pairs({
+		BattlegroundShine,
+		GameTimeFrame,
+		MinimapBorder,
+		MinimapBorderTop,
+		MinimapNorthTag,
+		MinimapZoomIn,
+		MinimapZoomOut,
+		MinimapZoneTextButton,
+		MiniMapBattlefieldBorder,
+		MiniMapLFGFrameBorder,
+		MiniMapTracking,
+		MiniMapWorldMapButton,
+	}) do
+		if(object:GetObjectType() == 'Texture') then
+			object:SetTexture(nil)
+		else
+			object:Hide()
+		end
+	end
 end
